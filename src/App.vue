80450a42ab8aedca652539fd1fc4f9df603f4a2f
falog <fal@falog.net>
2022-11-08 18:45:39 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrap">
+    <div>ヘッダー</div>
+    <main class="main">メイン</main>
+    <div>フッター</div>
+  </div>
+</template>
+
+<style>
+@import 'assets/css/destyle.css';
+
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  width: 370px;
+  margin: 0 auto;
+  font-family: sans-serif;
+}
+
+.main {
+  flex: 1;
+  width: 100%;
+}
+</style>
